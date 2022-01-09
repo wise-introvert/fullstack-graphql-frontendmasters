0da7ea799a149a10fa0a935285d37b9dadefa33a
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2022-01-09 16:41:35 -0500

    feat: ex 2: input types

--- api/src/schema.js
+++ api/src/schema.js
@@ -16,8 +16,14 @@ const typeDefs = gql`
     type: String!
   }
 
+  input PetInput {
+    name: String
+    type: String
+  }
+
   type Query {
-    pets: [Pet]!
+    pets(input: PetInput): [Pet]!
+    pet(input: PetInput): Pet!
   }
 `;
 
